f80d9bf92a4f1de36d7e41a0e00b3b0d661d85bb
Thomas Lavend'Homme <30930211+lavendthomas@users.noreply.github.com>
2018-04-19 19:37:51 +0200

    Added BEST rule (missing files)

---
+++ src/be/ac/umons/babaisyou/game/ActionBest.java
@@ -0,0 +1,22 @@
+package be.ac.umons.babaisyou.game;
+
+public class ActionBest extends Action {
+	
+	@Override
+	public boolean isBest() {
+		return true;
+	}
+
+	@Override
+	public void execute(Block block, Position player_position, Direction player_direction) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onEachTour(BlockType type) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
