8553450d71afbf35a8720f9fe28879e6fe42229f
Thomas Lavend'Homme <30930211+lavendthomas@users.noreply.github.com>
2018-05-12 14:51:47 +0200

    Launch script check for ant and java

--- babaisyou.sh
+++ babaisyou.sh
@@ -3,6 +3,17 @@
 # Ce script compile BabaIsYou si nécéssaire puis le lance.
 # Java 8 et Apache Ant sont nécéssaires.
 
+# Check if ant is installed 
+if ! [ -x "$(command -v ant)" ]; then
+    echo 'Error: ant is not installed.' >&2
+    exit 1
+fi
+
+# Check if java is installed 
+if ! [ -x "$(command -v java)" ]; then
+    echo 'Error: java is not installed.' >&2
+    exit 1
+fi
 
 
 # Compile si nésséssaire, c'est à dire si le dossier bin n'est
